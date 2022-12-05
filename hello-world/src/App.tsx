ee4968c8e4d58853368435ad4a819819d6eb287c
elizabeth.hastings121@gmail.com <elizabeth.hastings121@gmail.com>
2022-12-05 11:18:02 -0300

    Fizemos os componentes de Tema e Postagem sem fazer funcionalidade

--- hello-world/src/App.tsx
+++ hello-world/src/App.tsx
@@ -6,9 +6,12 @@ import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
-import TabPostagem from './components/postagens/tabpostagem/TabPostagem';
 import ListaTema from './components/temas/listatema/ListaTema';
 import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
+import CadastroPost from './components/postagens/cadastroPost/CadastroPost';
+import CadastroTema from './components/temas/cadastroTema/CadastroTema';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
 
 function App() {
   return (
@@ -21,6 +24,12 @@ function App() {
         <Route path='/cadastrousuario' element={<CadastroUsuario />} />
         <Route path='/temas' element={<ListaTema />} />
         <Route path='/postagem' element={<ListaPostagem />} />
+        <Route path="/formularioPostagem" element={<CadastroPost />} />
+        <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+        <Route path="/formularioTema" element={<CadastroTema />} />
+        <Route path="/formularioTema/:id" element={<CadastroTema />} />
+        <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+        <Route path="/deletarTema/:id" element={<DeletarTema />} />
       </Routes>
       <Footer />
     </Router>
--- hello-world/src/components/estaticos/navbar/Navbar.tsx
+++ hello-world/src/components/estaticos/navbar/Navbar.tsx
@@ -5,8 +5,17 @@ import { Link, useNavigate } from 'react-router-dom';
 import useLocalStorage from 'react-use-localstorage';
 import './Navbar.css'
 
-
 function Navbar() {
+
+    const [token, setToken] = useLocalStorage('token')
+    let navigate = useNavigate()
+
+    function goLogout(){
+        setToken('')
+        alert("Usuario deslogado com sucesso!")
+        navigate('/login')
+    }
+
     return (
         <>
             <AppBar position="static">
@@ -17,27 +26,35 @@ function Navbar() {
                         </Typography>
                     </Box>
                     <Box display="flex" justifyContent="start">
+                        <Link to='/home' className='text-decorator-none'>
                         <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 home
                             </Typography>
                         </Box>
+                        </Link>
+                        <Link to='/postagem' className='text-decorator-none'>
                         <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 postagens
                             </Typography>
                         </Box>
+                        </Link>
+                        <Link to='/temas' className='text-decorator-none'>
                         <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 temas
                             </Typography>
                         </Box>
+                        </Link>
+                        <Link to='/formulariotema' className='text-decorator-none'>
                         <Box mx={1} className='cursor'>
                             <Typography variant="h6" color="inherit">
                                 cadastrar tema
                             </Typography>
                         </Box>
-                        <Box mx={1} className='cursor'>
+                        </Link>
+                        <Box mx={1} className='cursor' onClick={goLogout}>
                             <Typography variant="h6" color="inherit">
                                 logout
                             </Typography>
